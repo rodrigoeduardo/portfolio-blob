fe9d66b98c0aba7f4666658a4bd2ccae285bdfd1
vinicius5g <vinicius.victor.sm@gmail.com>
2021-09-07 13:50:31 -0300

    initial structure

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {},
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
+  styles: {
+    global: {},
+  },
+});
