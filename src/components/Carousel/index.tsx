f5575df8afff9782f36167ca93c95a00b871a5a4
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-09-13 00:05:56 -0300

    py 20vh from 30vh

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -3,7 +3,7 @@ import { Slider } from './Slider';
 
 export function Carousel() {
   return (
-    <Box align="center" py="30vh">
+    <Box align="center" py="20vh">
       <Slider />
     </Box>
   );
