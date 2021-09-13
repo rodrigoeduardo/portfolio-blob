1d9985201b0b102ddf20c6e86c265bd29ed33282
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-09-13 00:06:50 -0300

    Contact component created

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,33 @@
+import { Stack } from "@chakra-ui/react";
+import { TeamMember } from "./TeamMember";
+
+export function Contact() {
+    return (
+        <>
+            <Stack direction="column" spacing="8rem">
+                <TeamMember
+                    avatarUrl="assets/images/teamMembers/rodrigo.jpg"
+                    name="Rodrigo Eduardo"
+                    role="Co-Founder & CEO"
+                    linkedinUrl="https://linkedin.com/in/rodrigoedb"
+                    githubUrl="https://github.com/rodrigoeduardo"
+                />
+
+                <TeamMember
+                    avatarUrl="assets/images/teamMembers/vinicius.jpg"
+                    name="Vinicius Victor"
+                    role="Co-Founder & CTO"
+                    linkedinUrl="https://linkedin.com/in/martins-vinicius"
+                    githubUrl="https://github.com/martinsvinicius"
+                />
+
+                <TeamMember
+                    avatarUrl="assets/images/teamMembers/mariaeduarda.jpg"
+                    name="Maria Eduarda"
+                    role="Co-Founder & Manager"
+                    linkedinUrl="https://www.linkedin.com/in/mariaeduluz"
+                />
+            </Stack>
+        </>
+    )
+}
