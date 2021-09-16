eafb27fd45f0d8dcb9c1ee72a39286df847d0caa
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-09-15 22:54:24 -0300

    fixing page title location

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Blob Team</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
