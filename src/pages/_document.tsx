57aa7ad54b2d388d9f8e3e312c10e69443315705
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-09-24 00:12:22 -0300

    testing og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,12 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="assets/icons/blob-circle-gray.svg" />
+
+          <meta property="og:url"                content="https://blobteam.tech" />
+          <meta property="og:type"               content="website" />
+          <meta property="og:title"              content="Blob Team - Portfólio" />
+          <meta property="og:description"        content="A web development team." />
+          <meta property="og:image"              content="https://media-exp1.licdn.com/dms/image/C4E0BAQFy68xj5OiETg/company-logo_200_200/0/1632447900442?e=1640822400&v=beta&t=pjqwNMWjrEl6nDfbD638LqijbhmTNQLi_xFzX_68YDo" />
         </Head>
 
         <body>
