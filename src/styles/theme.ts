9a8db91c1f2b984765c4da0733d72f2459cf915c
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-09-23 21:38:43 -0300

    breakpoints added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,17 @@
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+  base: "0px",
+  xsm: "360px",
+  sm: "380px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1400px",
+})
 
 export const theme = extendTheme({
+  breakpoints,
   colors: {
     gray: {
       '900': '#0D0712',
