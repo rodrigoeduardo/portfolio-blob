ebde0df215366abcf4f5cb80ac0fc0bafe8cd4d7
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-09-23 21:41:15 -0300

    ContactFormSection responsiveness

--- src/components/Contact/ContactFormSection.tsx
+++ src/components/Contact/ContactFormSection.tsx
@@ -3,7 +3,7 @@ import { Form } from "./Form";
 
 export function ContactFormSection() {
     return (
-        <Flex align="center" flexDir="column" bg="purple.800" py="4.2rem" px="7rem" borderRadius="20px">
+        <Flex align="center" flexDir="column" bg="purple.800" py="4.2rem" px={{ base: "4rem", sm: "7rem" }} my={{ base: "8rem", md: "0" }} borderRadius="20px">
             <Text
                 fontSize="3.6rem"
                 fontWeight="bold"
